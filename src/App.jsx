c7b70cb1a254be951d8360e4cb48316e1974510c
Salvador De la Tijera <106124460+SalvadorDelaTijera@users.noreply.github.com>
2024-03-01 12:02:01 -0600

    api personas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState();
+  const datos = async () => {
+    let res = await fetch("https://randomuser.me/api/?results=30");
+    res = await res.json();
+    setData(res);
+  };
+  useEffect(() => {
+    datos();
+  }, []);
+  console.log(data);
 
-export default App
+  return (
+    <div>
+      {data?.results?.map((i) => {
+        return (
+          <ul>
+            <img src={i.picture.large} alt="" />
+            <h4>
+              {i.name.first} {i.name.last}
+            </h4>
+            <p>{i.id.name}</p>
+            <p>{i.dob.age}</p>
+            <p>{i.gender}</p>
+            <p>{i.phone}</p>
+            <p>{i.cell}</p>
+            <p>{i.email}</p>
+            <p>{i.registered.age}</p>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
+
+
+/**
+ * <ul>
+            <img src={i.picture.large} alt="" />
+            <h4>
+              {i.name.first} {i.name.last}
+            </h4>
+            <p>{i.id.name}</p>
+            <p>{i.dob.age}</p>
+            <p>{i.gender}</p>
+            <p>{i.phone}</p>
+            <p>{i.cell}</p>
+            <p>{i.email}</p>
+            <p>{i.registered.age}</p>
+          </ul>
+ */
